d3c1be22cd75f2267bbd4b98a2f800a4ce851b61
yinghaidada <pyj2897022134@gmail.com>
2023-10-21 19:22:53 +0800

    优化LayerStack代码，确保弹出和销毁时调用OnDetach

--- CraftNow/src/CraftNow/LayerStack.cpp
+++ CraftNow/src/CraftNow/LayerStack.cpp
@@ -11,7 +11,10 @@ namespace CraftNow
 	LayerStack::~LayerStack()
 	{
 		for (Layer *layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer *layer)
@@ -27,18 +30,22 @@ namespace CraftNow
 
 	void LayerStack::PopLayer(Layer *layer)
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-		if (it != m_Layers.end())
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
 		}
 	}
 
-	void LayerStack::PopOverlay(Layer *layer)
+	void LayerStack::PopOverlay(Layer * overlay)
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+		auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
 		if (it != m_Layers.end())
+		{
+			overlay->OnDetach();
 			m_Layers.erase(it);
+		}
 	}
 }
