5faf5705b0b08991f95356b77535d611107a1cc8
yinghaidada <pyj2897022134@gmail.com>
2023-11-03 16:10:38 +0800

    添加Debug支持

--- CraftNow/src/cnpch.h
+++ CraftNow/src/cnpch.h
@@ -1,5 +1,12 @@
 ﻿#pragma once
 
+#ifdef CN_PLATFORM_WINDOWS
+	#ifndef NOMINMAX
+		// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+		#define NOMINMAX
+	#endif
+#endif
+
 #include <iostream>
 #include <memory>
 #include <utility>
@@ -13,8 +20,10 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include "CraftNow/Core/Core.h"
 #include "CraftNow/Core/Log.h"
 
+#include "CraftNow/Debug/Instrumentor.h"
 
 #ifdef  CN_PLATFORM_WINDOWS
 	#include <Windows.h>
