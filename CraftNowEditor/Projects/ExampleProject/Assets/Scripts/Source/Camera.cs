f93f7671bfebce63f6607c524260c1005b359516
yinghaidada <pyj2897022134@gmail.com>
2023-11-30 16:17:20 +0800

    添加Zelda测试场景

--- CraftNowEditor/Projects/ExampleProject/Assets/Scripts/Source/Camera.cs
+++ CraftNowEditor/Projects/ExampleProject/Assets/Scripts/Source/Camera.cs
@@ -41,6 +41,50 @@ namespace Sandbox
 
 			velocity *= speed;
 
+			/*Vector3 translation = Translation;
+			translation += velocity * ts;
+			Translation = translation;*/
+		}
+
+	}
+}
+
+namespace Zelda
+{
+	public class Camera : Entity
+	{
+		public Entity OtherEntity;
+
+		public float DistanceFromPlayer = 5.0f;
+
+		private Entity m_Player;
+
+		void OnCreate()
+		{
+			m_Player = FindEntityByName("YINGHAI");
+		}
+
+		void OnUpdate(float ts)
+		{
+
+			if (m_Player != null)
+				Translation = new Vector3(m_Player.Translation.XY, DistanceFromPlayer);
+
+			float speed = 1.0f;
+			Vector3 velocity = Vector3.Zero;
+
+			if (Input.IsKeyDown(KeyCode.Up))
+				velocity.Y = 2.0f;
+			else if (Input.IsKeyDown(KeyCode.Down))
+				velocity.Y = -2.0f;
+
+			if (Input.IsKeyDown(KeyCode.Left))
+				velocity.X = -2.0f;
+			else if (Input.IsKeyDown(KeyCode.Right))
+				velocity.X = 2.0f;
+
+			velocity *= speed;
+
 			Vector3 translation = Translation;
 			translation += velocity * ts;
 			Translation = translation;
--- CraftNowEditor/Projects/ExampleProject/Assets/Scripts/Source/Player.cs
+++ CraftNowEditor/Projects/ExampleProject/Assets/Scripts/Source/Player.cs
@@ -83,3 +83,86 @@ namespace Sandbox
 
 	}
 }
+
+namespace Zelda
+{
+	public class Player : Entity
+	{
+		private TransformComponent m_Transform;
+		private Rigidbody2DComponent m_Rigidbody;
+
+		public float Speed;
+		public float Time = 0.0f;
+
+		void OnCreate()
+		{
+			Console.WriteLine($"Script: Player.OnCreate - {ID}");
+
+			m_Transform = GetComponent<TransformComponent>();
+			m_Rigidbody = GetComponent<Rigidbody2DComponent>();
+		}
+
+		void OnUpdate(float ts)
+		{
+			// Console.WriteLine($"Player.OnUpdate: {ts}");
+			Time += ts;
+
+			float speed = 0.01f;
+			Vector3 velocity = Vector3.Zero;
+
+			//Vector3 translation = Translation;
+
+			if (Input.IsKeyDown(KeyCode.W))
+			{
+				velocity.Y = 2.0f;
+				//translation.Y += 2.0f;
+				Console.WriteLine("W");
+			}
+			else if (Input.IsKeyDown(KeyCode.S))
+			{
+				velocity.Y = -2.0f;
+				//translation.Y -= 2.0f;
+				Console.WriteLine("S");
+			}
+
+			if (Input.IsKeyDown(KeyCode.A))
+			{
+				velocity.X = -2.0f;
+				//translation.X -= 1.0f;
+				Console.WriteLine("A");
+			}
+			else if (Input.IsKeyDown(KeyCode.D))
+			{
+				velocity.X = 2.0f;
+				//translation.X += 1.0f;
+				Console.WriteLine("D");
+			}
+
+			Entity cameraEntity = FindEntityByName("Camera");
+			if (cameraEntity != null)
+			{
+				Camera camera = cameraEntity.As<Camera>();
+
+				if (Input.IsKeyDown(KeyCode.Q))
+				{
+					camera.DistanceFromPlayer += speed * 2.0f;
+					Console.WriteLine("Q press");
+				}
+				else if (Input.IsKeyDown(KeyCode.E))
+				{
+					camera.DistanceFromPlayer -= speed * 2.0f;
+					Console.WriteLine("E press");
+				}
+			}
+
+			velocity *= speed;
+
+			//m_Rigidbody.ApplyLinearImpulse(velocity.XZ, true);
+
+			Vector3 translation = Translation;
+			translation += velocity;
+			Translation = translation;
+		}
+
+	}
+}
